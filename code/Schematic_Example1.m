f8584ded441dcd3dae29c379a6928690f1ca308d
Hosky125 <hosky125@163.com>
2025-06-23 14:50:31 +0800

    Update Schematic_Example1.m

--- code/Schematic_Example1.m
+++ code/Schematic_Example1.m
@@ -1,15 +1,31 @@
+%diagonal coupling network
+%-----------------------------------------------------%
+%module partition by multilayer algorithm
+%without interlayer links weight
 %Figure 1(a)
 module_partition=[1 1;1 1]
 HomoMI("multilayer",module_partition,0)
+
+%module partition by multilayer algorithm
+%without interlayer links weight
 %Figure 1(b)
 module_partition=[1 1;1 2]
 HomoMI("multilayer",module_partition,0)
+
+%module partition by multilayer algorithm
+%without interlayer links weight
 %Figure 1(c)
 module_partition=[1 1;2 3]
 HomoMI("multilayer",module_partition,0)
+
+%module partition by multilayer algorithm
+%without interlayer links weight
 %Figure 1(d)
 module_partition=[1 1;2 2]
 HomoMI("monolayer",module_partition,0)
+
+%module partition by multilayer algorithm
+%without interlayer links weight
 %Figure 1(e)
 module_partition=[1 1;2 3]
 HomoMI("monolayer",module_partition,0)
