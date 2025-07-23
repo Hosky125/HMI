77e4e29df1b116b266695dff47e13b004464544a
Hosky125 <132581033+Hosky125@users.noreply.github.com>
2025-07-23 14:45:36 +0800

    Update SC_Example.m

--- code/SC_Example.m
+++ code/SC_Example.m
@@ -5,13 +5,14 @@ layer=2;
 Intra_data=cell(1,layer);
 
 %import data
-SC_fileInfo = dir(fullfile(pwd, 'DATA/First/SC/','*.csv'));
+SC_fileInfo = dir(fullfile(pwd, 'DATA/First/SC/','*.txt'));
 SC_fileNames = {SC_fileInfo.name}';
 
 %A worked example of calculating HMI
 % 构建完整的文件路径
 SC_filePath1 = fullfile(pwd,'DATA/First/SC/',SC_fileNames(1));
 SC_filePath2 = fullfile(pwd,'DATA/First/SC/',SC_fileNames(2));
+SC_filePath3 = fullfile(pwd,'DATA/First/SC/',SC_fileNames(3));
 
 % 读取文件
 PH=importdata(SC_filePath1{1});
@@ -20,7 +21,9 @@ Intra_data{1}=PH.data;
 PP=importdata(SC_filePath2{1});
 Intra_data{2}=PP.data;
 
-Inter_data=[0.053 0.068 0.014 0.061 0.019 0.147 0.083 0.107 0.065];
+Inter_data=importdata(SC_filePath3{1});
+Inter_data=Inter_data.data;
+%Inter_data=[0.053 0.068 0.014 0.061 0.019 0.147 0.083 0.107 0.065];
 %-------------------------------------------------------------------------%
 
 %module partition by monolayer algorithm
